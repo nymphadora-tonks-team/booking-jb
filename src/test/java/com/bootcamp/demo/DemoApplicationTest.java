e4a26707c2c0453a78d865b3f81dea1c5c1bde8c
Nymphadora Tonks Team <nymphadora.tonks.team@gmail.com>
2021-10-11 22:31:01 +0300

    initial commit

---
+++ src/test/java/com/bootcamp/demo/DemoApplicationTest.java
@@ -0,0 +1,12 @@
+package com.bootcamp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
