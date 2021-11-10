66ffb925ea09fdba4bcedab2dbe60ae406e10b36
Bernat <domberills@gmai.com>
2021-11-10 20:04:08 +0200

    added abstract model, for storing and generating random uuid for each model

--- src/main/java/com/bootcamp/demo/model/Scooter.java
+++ src/main/java/com/bootcamp/demo/model/Scooter.java
@@ -1,22 +1,22 @@
 package com.bootcamp.demo.model;
 
+import com.bootcamp.demo.model.util.AbstractModel;
 import com.bootcamp.demo.model.util.Location;
 import com.bootcamp.demo.model.util.Battery;
 import com.bootcamp.demo.model.util.ScooterStatus;
 
-import java.io.Serializable;
 import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
 
-public class Scooter implements Serializable {
+public class Scooter extends AbstractModel {
     private Location currentLocation;
     private Battery battery;
     private ScooterStatus status;
 
     private Scooter() {}
 
-    public Scooter(String id, Location currentLocation, Battery battery, ScooterStatus status) {
+    public Scooter(Location currentLocation, Battery battery, ScooterStatus status) {
         this.currentLocation = requireNonNull(currentLocation);
         this.battery = requireNonNull(battery);
         this.status = requireNonNull(status);
@@ -58,13 +58,14 @@ public class Scooter implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Scooter)) return false;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
         Scooter scooter = (Scooter) o;
-        return currentLocation.equals(scooter.currentLocation) && battery.equals(scooter.battery) && status == scooter.status;
+        return getCurrentLocation().equals(scooter.getCurrentLocation()) && getBattery().equals(scooter.getBattery()) && getStatus() == scooter.getStatus();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(currentLocation, battery, status);
+        return Objects.hash(super.hashCode(), getCurrentLocation(), getBattery(), getStatus());
     }
 }
