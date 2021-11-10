eb865187e7bb5085af59dade2b7a40b510feffa1
Furcovici Teodora <teodora.furcovici@yahoo.com>
2021-11-10 08:52:06 +0200

    Update SecurityConfig.java

--- src/main/java/com/bootcamp/demo/security/SecurityConfig.java
+++ src/main/java/com/bootcamp/demo/security/SecurityConfig.java
@@ -5,7 +5,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+/*
+partea de securitate a applicatiei
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
