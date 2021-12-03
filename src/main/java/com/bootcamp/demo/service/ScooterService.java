5bc0e68840bac005e010fddbd7e6af82d6c77e5c
Bernat <domberills@gmail.com>
2021-12-04 00:10:41 +0200

    removed the common-lag3 dependency + usages

--- src/main/java/com/bootcamp/demo/service/ScooterService.java
+++ src/main/java/com/bootcamp/demo/service/ScooterService.java
@@ -44,7 +44,7 @@ public class ScooterService {
 
             return scooter.toObject(Scooter.class);
         } catch (ExecutionException | InterruptedException | IllegalArgumentException e) {
-            LOGGER.error("FIND SCOOTER BY ID - service function: scooterId = {}.\n Error message: {}.\n StackTrace: {}", scooterId, e.getMessage(), ExceptionUtils.getStackTrace(e));
+            LOGGER.error("FIND SCOOTER BY ID - service function: scooterId = {}.\n Error message: {}.", scooterId, e.getMessage(), e);
             throw new ServiceException(e);
         }
     }
@@ -61,7 +61,7 @@ public class ScooterService {
                     .map(queryDocumentSnapshot -> queryDocumentSnapshot.toObject(Scooter.class))
                     .collect(Collectors.toSet());
         } catch (ExecutionException | InterruptedException e) {
-            LOGGER.error("FIND ALL SCOOTER - service function. Error message: {}.\n StackTrace: {}", e.getMessage(), ExceptionUtils.getStackTrace(e));
+            LOGGER.error("FIND ALL SCOOTER - service function. Error message: {}.", e.getMessage(), e);
             throw new ServiceException(e);
         }
 
@@ -79,7 +79,7 @@ public class ScooterService {
 
             LOGGER.info("CREATE SCOOTER - created successfully. scooter = {}. Update_time: {}", scooter, update_time);
         } catch (ExecutionException | InterruptedException e) {
-            LOGGER.error("CREATE SCOOTER - service function: scooter = {}.\n Error message: {}.\n StackTrace: {}", scooter, e.getMessage(), ExceptionUtils.getStackTrace(e));
+            LOGGER.error("CREATE SCOOTER - service function: scooter = {}.\n Error message: {}.", scooter, e.getMessage(), e);
             throw new ServiceException(e);
         }
     }
