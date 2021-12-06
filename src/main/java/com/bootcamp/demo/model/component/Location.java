cfa8865ef219d0076dc1002a6a420f386977beeb
Furcovici Teodora <teodora.furcovici@yahoo.com>
2021-12-06 18:31:07 +0200

    empty constructor added for Location class

--- src/main/java/com/bootcamp/demo/model/component/Location.java
+++ src/main/java/com/bootcamp/demo/model/component/Location.java
@@ -6,6 +6,9 @@ public final class Location {
     private Double latitude;
     private Double longitude;
 
+    public Location() {
+    }
+
     public Location(Double latitude, Double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
